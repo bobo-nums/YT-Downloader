802fe042ad90f5e0001ee6c82b04cedca7ac298b
Bowen Xiao <xiao.bowen18@gmail.com>
2020-12-28 19:48:23 -0500

    TODO: prev & incorrect title format

--- main.py
+++ main.py
@@ -69,7 +69,8 @@ for x, video in enumerate(video_list):
         else:
             print("720p 30fps available")
     
-    title = v.title
+    title = input("Enter video title: ")
+    # title = v.title
     print(f"Downloading " + msg + " #" + str(x + 1) + "...")
     output = os.getcwd() + "\Downloads"
     stream.download(filename=title, output_path=output)
